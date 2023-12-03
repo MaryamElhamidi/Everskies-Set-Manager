83b278b9b4e4b507e1b922d5bb795ca98b4c73be
MaryamElhamidi <unicolmaryame@gmail.com>
2023-12-03 00:35:15 -0500

    Resource Manager & Data Persistence Rough

--- ResourceManager.py
+++ ResourceManager.py
@@ -1,33 +1,47 @@
-# resource_manager.py
+from Resource import Resource
+import json
+
 
 class ResourceManager:
     def __init__(self):
         self.resources = []
 
-    def create_resource(self, attribute1, attribute2):
-        # Create a new resource object
-        pass
 
-    def search_resources(self, key_attribute, non_key_attribute):
-        # Search for resources based on key and non-key attributes
-        pass
+    def create_resource(self, id, attribute1, attribute2): # Creates a new resource object
+        resource = Resource(id, attribute1, attribute2)
+        return resource
+    
 
-    def update_resource(self, resource_id, new_attribute1, new_attribute2):
-        # Update the attributes of a resource
-        pass
+    def search_resources(self, key_attribute, non_key_attribute): # Search for resources based on key and non-key attributes
+        found_resources = []
+        for resource in self.resources:
+            if getattr(resource, key_attribute, None) == non_key_attribute:
+                found_resources.append(resource)
+        return found_resources
 
-    def delete_resource(self, resource_id):
-        # Delete a resource based on its ID
-        pass
-# data_persistence.py
+    def update_resource(self, resource_id, new_attribute1, new_attribute2):# Update the attributes of a resource
 
-import json
+        for resource in self.resources:
+            if resource.id == resource_id:
+                resource.attribute1 = new_attribute1
+                resource.attribute2 = new_attribute2
+
+    def delete_resource(self, resource_id): # Delete a resource based on its ID
+        self.resources = [resource for resource in self.resources if resource.id != resource_id]
+
+# Data Persistence
 
 class DataPersistence:
-    def load_data(self, filename):
-        # Load data from a file (JSON or CSV)
-        pass
+    def load_data(self, filename): # Load data from a file (JSON or CSV)
+        try:
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                return [Resource(**item) for item in data]
+        except (FileNotFoundError, json.JSONDecodeError):
+            return []        
 
-    def save_data(self, filename, data):
-        # Save data to a file (JSON or CSV)
-        pass
+    def save_data(self, filename, data): # Save data to a file (JSON or CSV)
+        with open(filename, 'w') as file:
+            serialized_data = [{'id': resource.id, 'attribute1': resource.attribute1, 'attribute2': resource.attribute2}
+                               for resource in data]
+            json.dump(serialized_data, file, indent=2)
