6e86c45a424a6ccd60f6e2435a1c6c05f1386d58
MaryamElhamidi <unicolmaryame@gmail.com>
2023-12-10 14:35:44 -0500

    Resource Class comments

--- Resource.py
+++ Resource.py
@@ -1,10 +1,10 @@
-# resource.py
+class Resource:# Constructor (Initializer) method for creating a new Resource object
+    def __init__(self, id, set_Designer, set_Name):  # Assign values to the attributes of the Resource object
+        self.id = id  # Unique identifier for the resource
+        self.set_Designer = set_Designer #This is the KEY ATTRIBUTE. It is the designer information for the resource.
+        self.set_Name = set_Name # This is the NON-key ATTRIBUTE. It is the set name information for the resource.
 
-class Resource:
-    def __init__(self, id, set_Designer, set_Name):
-        self.id = id
-        self.set_Designer = set_Designer
-        self.set_Name = set_Name
-
-    def __str__(self):
+    def __str__(self):   # String representation method for the Resource object
         return f"Set ID: {self.id}, Designer: {self.set_Designer}, Set Name: {self.set_Name}"
+        # Returns a formatted string representing the Resource object
+
